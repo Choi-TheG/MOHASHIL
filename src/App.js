90ed6a120240b9d4fe63fe96175c939d4ef71e37
Choi-TheG <choiejwl187@gmail.com>
2023-01-31 23:15:09 +0900

    '0131'

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import DrawButton from "./components/UI/DrawButton";
 
 function App() {
   return (
-    <div>
-      <div style={{ border: 1 }}>
+    <div style={{ width: "80%", margin: "0px auto" }}>
+      <div style={{ margin: "0px auto" }}>
         <div>
-          <button>❌</button>
-          <button style={{ float: "right" }}>⭐</button>
+          <select style={{ float: "right" }}>
+            <option>KOR</option>
+            <option>ENG</option>
+          </select>
         </div>
-        <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            height: "30px",
+          }}
+        >
           <input type="radio" name="category" />
           카페
           <input type="radio" name="category" />
@@ -21,17 +30,33 @@ function App() {
           <input type="radio" name="category" />
           배달
         </div>
-        <select>
-          <option>cafe</option>
-          <option>activity</option>
-          <option>food</option>
-          <option>alcohol</option>
-          <option>delivery</option>
-        </select>
+        <div>
+          <button
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 0)",
+              border: "1px, solid",
+              borderRadius: "5px",
+            }}
+          >
+            ❌
+          </button>
+          <button
+            style={{
+              float: "right",
+              backgroundColor: "rgba(255, 255, 255, 0)",
+              border: "1px, solid",
+              borderRadius: "5px",
+            }}
+          >
+            ⭐
+          </button>
+        </div>
         <h1>img</h1>
         <DrawButton>랜덤추천</DrawButton>
-        <input type="checkbox" value="오늘은 보지 않기" />
-        오늘은 보지 않기
+        <button style={{ float: "center" }} className="todayBtn">
+          <input type="checkbox" value="오늘은 보지 않기" />
+          오늘은 보지 않기
+        </button>
       </div>
     </div>
   );
--- src/components/UI/DrawButton.js
+++ src/components/UI/DrawButton.js
@@ -2,11 +2,30 @@ import React, { useState } from "react";
 import classes from "./DrawButton.module.css";
 
 const DrawButton = (props) => {
-  const [a, setA] = useState();
+  const [oneMore, setOneMore] = useState(props.children);
+  const [count, setCount] = useState(0);
+
+  const randomButtonHandler = () => {
+    const JJIN = "찐";
+    const MAK = "막?";
+    if (count === 0) {
+      setOneMore(JJIN + MAK);
+      setCount(count + 1);
+    } else if (count < 5) {
+      setOneMore(JJIN + oneMore);
+      setCount(count + 1);
+    } else if (count === 5) {
+      setOneMore("ㄹㅇ 찐막 ㄱ");
+    }
+  };
+
   console.log(props.children);
   return (
-    <button className={`${classes.DrawButton} ${classes.Button}`}>
-      {props.children}
+    <button
+      className={`${classes.DrawButton} ${classes.Button}`}
+      onClick={randomButtonHandler}
+    >
+      {oneMore}
     </button>
   );
 };
